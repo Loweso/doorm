c5bde1002d27fe62614baac90618c9f47f962d33
powtzulist <delasanarwin@gmail.com>
2024-04-21 06:11:21 +0800

    supabase authentication and route w/o frontend

---
+++ app/auth/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import supabaseBrowserClient from "@/utils/supabase/client";
+
+export default function Login() {
+  const loginWithGoogle = () => {
+    const supabase = supabaseBrowserClient();
+    supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: location.origin + "auth/callback",
+      },
+    });
+  };
+
+  return (
+    <div className="h-20 w-20">
+      <button onClick={loginWithGoogle}>ADSADSA</button>
+    </div>
+  );
+}
