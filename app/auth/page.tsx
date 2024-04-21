f214e8c8854796d8b684850bb227c35eb4c54a60
powtzulist <delasanarwin@gmail.com>
2024-04-21 08:46:28 +0800

    added a drop shadow in google sign in box

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -17,7 +17,7 @@ export default function Login() {
   return (
     <div className="h-screen font-poppins bg-bgColor flex flex-col justify-between ">
       <div className="flex-grow flex justify-center items-center">
-        <div className="w-[25%] h-[40%] rounded-xl border-[1px] border-black bg-accentColor-lightBlue bg-opacity-40 flex flex-col space-y-8 pt-14">
+        <div className="w-[25%] h-[40%] rounded-xl border-[1px] border-black bg-accentColor-lightBlue bg-opacity-40 flex flex-col space-y-8 pt-14 shadow-lg">
           <div className="flex justify-center">
             <Image
               src="/logo/doorm.png"
