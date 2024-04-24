502b26bd625858b19b8a512a45b0945a6c506610
Bettina Ligero <ligerobc24@gmail.com>
2024-04-24 18:10:01 +0800

    created a applicants row component which is utilized in the applications page

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -4,15 +4,16 @@ import { FiEdit3 } from "react-icons/fi";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
+import AppRow from "@/components/applicantsRow/appRow";
 
 const page = () => {
   return (
-    <div className="flex flex-row justify-center items-center bg-[#FEFBF6]">
+    <div className="flex flex-row justify-center items-center bg-[#FEFBF6] p-2">
         <div className="w-5/6 bg-[#ECB159] border-[1px] rounded-lg border-gray-800"> 
-            <div className="h-4/6 bg-[#EFEBE3] border-b-[1px] rounded-lg border-gray-800 flex flex-col justify-start items-center gap-4">
-                <div className="w-full h-5/6 bg-[#FEFBF6] border-b-[1px] rounded-lg border-gray-800 flex flex-row"> 
+            <div className="h-4/6 bg-[#EFEBE3] border-b-[1px] rounded-lg border-gray-800 flex flex-col justify-center items-center">
+                <div className="w-full h-5/6 bg-[#FEFBF6] border-b-[1px] rounded-lg border-gray-800 flex flex-row justify-center items-center"> 
                     <div className="w-1/2 rounded-lg flex justify-center items-center"> 
-                        <div className="w-5/6 h-5/6 border-[1px] rounded-lg border-gray-800"> 
+                        <div className="w-5/6 h-5/6 border-[1px] rounded-lg border-gray-800 m-2"> 
                             <Image
                             src="/shrekid.jpg"
                             width={500}
@@ -21,7 +22,7 @@ const page = () => {
                             className="w-full h-full border-[1px] rounded-lg border-gray-800"/>
                         </div>                    
                     </div>
-                    <div className="w-1/2 h-full rounded-lg flex justify-center items-start flex-col gap-12"> 
+                    <div className="w-1/2 h-full rounded-lg flex justify-center items-start flex-col gap-8"> 
                         <h1 className="text-3xl text-[#4E423B] font-semibold"> Name </h1>
                         <div className="w-full h-1/3 flex justify-start items-center flex-row gap-4"> 
                             <button className="w-32 h-12 bg-[#ECB159] bg-opacity-35 border-[1px] rounded-lg border-gray-800 flex justify-center items-center hover:bg-[#CCB89A]"> 
@@ -35,66 +36,28 @@ const page = () => {
                         </div>
                     </div>
                 </div>
-                <div> 
+                <div className="m-2"> 
                     <p className="text-2xl text-[#4E423B] font-semibold"> List of Applicants </p>
                 </div>
             </div>
-            <div className="flex flex-row overflow-y-auto"> 
-                <div className="w-1/3 h-full flex flex-col items-center m-2 gap-2"> 
+            <div className="flex flex-row"> 
+                <div className="w-1/3 h-full flex flex-col items-center m-2"> 
                     <p className="text-base text-[#4E423B] font-semibold"> Full Name: </p>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
                 </div>
-                <div className="w-1/3 h-full flex flex-col items-center m-2 gap-2"> 
+                <div className="w-1/3 h-full flex flex-col items-center m-2"> 
                     <p className="text-base text-[#4E423B] font-semibold"> Price Offer (in Php):  </p>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
-                    <div className=" w-80 h-10 bg-[#EFEBE3] rounded"> </div>
                 </div>
                 <div className="w-1/3 h-full flex flex-col items-center m-2 gap-2"> 
                     <p className="text-base text-[#4E423B] font-semibold"> Your Decision: </p>
-                    <div className="flex flex-row gap-4"> 
-                        <div className="w-1/2 flex flex-col justify-center items-center gap-2"> 
-                            <button className="w-36 h-10 bg-[#8CB9BD] flex justify-center items-center rounded hover:bg-[#905B40]"> 
-                            <FaCheck color="white" className="w-9 h-9 "/> </button>
-                            <button className="w-36 h-10 bg-[#8CB9BD] flex justify-center items-center rounded"> 
-                            <FaCheck color="white" className="w-9 h-9 "/> </button>
-                            <button className="w-36 h-10 bg-[#8CB9BD] flex justify-center items-center rounded"> 
-                            <FaCheck color="white" className="w-9 h-9 "/> </button>
-                            <button className="w-36 h-10 bg-[#8CB9BD] flex justify-center items-center rounded"> 
-                            <FaCheck color="white" className="w-9 h-9 "/> </button>
-                            <button className="w-36 h-10 bg-[#8CB9BD] flex justify-center items-center rounded"> 
-                            <FaCheck color="white" className="w-9 h-9 "/> </button>
-                            <button className="w-36 h-10 bg-[#8CB9BD] flex justify-center items-center rounded"> 
-                            <FaCheck color="white" className="w-9 h-9 "/> </button>
-                            
-                        </div>
-                        <div className="w-1/2 flex flex-col justify-center items-center gap-2"> 
-                            <button className="w-36 h-10 bg-[#B67352] flex justify-center items-center rounded"> 
-                            <ImCross color="white" className="w-8 h-8 "/> </button>
-                            <button className="w-36 h-10 bg-[#B67352] flex justify-center items-center rounded"> 
-                            <ImCross color="white" className="w-8 h-8 "/> </button>
-                            <button className="w-36 h-10 bg-[#B67352] flex justify-center items-center rounded"> 
-                            <ImCross color="white" className="w-8 h-8 "/> </button>
-                            <button className="w-36 h-10 bg-[#B67352] flex justify-center items-center rounded"> 
-                            <ImCross color="white" className="w-8 h-8 "/> </button>
-                            <button className="w-36 h-10 bg-[#B67352] flex justify-center items-center rounded"> 
-                            <ImCross color="white" className="w-8 h-8 "/> </button>
-                            <button className="w-36 h-10 bg-[#B67352] flex justify-center items-center rounded"> 
-                            <ImCross color="white" className="w-8 h-8 "/> </button>
-
-                        </div>
-                    </div>
                 </div>
-
             </div>
+            <AppRow/>
+            <AppRow/>
+            <AppRow/>
+            <AppRow/>
+            <AppRow/>
+            <AppRow/>
+            <AppRow/>
         </div>
     </div>
   )
