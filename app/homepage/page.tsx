c1e7203fa5c4c57984cdbaeccb257cf80e15fde5
Renier Jardio <renierjardio@gmail.com>
2024-04-24 16:03:34 +0800

    made searchbar component

---
+++ app/homepage/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Searchbar } from "@/components/search/searchbar";
+
+export default function page() {
+  return (
+    <div className="flex justify-center">
+      <Searchbar />
+    </div>
+  );
+}
