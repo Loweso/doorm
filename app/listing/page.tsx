109870124daa7a2ade96e6ad51fabfde425b87e6
powtzulist <delasanarwin@gmail.com>
2024-04-24 16:50:04 +0800

    listings page

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { DormCardContainer } from "@/components/DormCardContainer";
+import { Searchbar } from "@/components/search/searchbar";
 
 export default function page() {
   return (
-    <main className="h-full flex flex-col bg-content-white">
-      <h1>Available Listings</h1>
+    <main className=" flex flex-col bg-content-white space-y-12">
+      <div className="flex justify-center">
+        <Searchbar />
+      </div>
+      <h1 className="ml-24 text-3xl font-bold text-accentColor-earthyYellow">
+        Available Listings
+      </h1>
       <DormCardContainer />
     </main>
   );
--- components/DormCardContainer.tsx
+++ components/DormCardContainer.tsx
@@ -1,9 +1,19 @@
 import { DormCard } from "./DormCard";
+import { FaHouse } from "react-icons/fa6";
 
 export const DormCardContainer = () => {
   return (
-    <div className="flex-1 flex justify-center">
+    <div className="grid grid-cols-3 place-items-center ps-16 pe-16 pb-12 gap-y-10 ">
       <DormCard />
+      <DormCard />
+      <DormCard />
+      <DormCard />
+      <DormCard />
+      <DormCard />
+      <h1 className="col-span-3 text-content-darkBrown font-semibold text-lg flex items-baseline gap-x-1 cursor-pointer">
+        <FaHouse />
+        More Dorms
+      </h1>
     </div>
   );
 };
