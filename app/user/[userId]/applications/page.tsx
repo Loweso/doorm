b4ec92f12aced9e65a4ae902c22da896c0325096
Renier Jardio <renierjardio@gmail.com>
2024-05-23 17:01:39 +0800

    added readApplications and updateApplications on user and listing applications

--- app/user/[userId]/applications/page.tsx
+++ app/user/[userId]/applications/page.tsx
@@ -1,13 +1,35 @@
+"use client";
 import { DormCardContainer } from "@/components/DormCardContainer";
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { userStore } from "@/store/userStore";
 
-function Applications({ params }) {
+function Applications({}) {
+  const user = userStore((state) => state.user);
+  const [dormListings, setDormListings] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchDormInfo = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/user/${user?.user_ID}/applications`
+        );
+        setDormListings(response.data);
+      } catch (error) {
+        console.error("Error fetching dorm information:", error);
+      }
+    };
+
+    fetchDormInfo();
+  }, []);
+
   return (
     <main className=" flex flex-col bg-content-white space-y-12">
       <h1 className="mt-12 ml-24 text-3xl font-bold text-accentColor-earthyYellow">
         Your Applications
       </h1>
-      <DormCardContainer />
+      <DormCardContainer dormListings={dormListings} />
     </main>
   );
 }
--- components/applicantsRow/appRow.tsx
+++ components/applicantsRow/appRow.tsx
@@ -1,21 +1,92 @@
+"use client";
 import React from "react";
+import { useEffect, useState } from "react";
 import { FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
+import axios from "axios";
 
-const AppRow = () => {
+interface Application {
+  User: { fullName: string };
+  rent: number;
+  status: string;
+  user_ID: string;
+  dormId: string;
+}
+
+interface AppRowProps {
+  application: Application;
+}
+
+const AppRow: React.FC<AppRowProps> = ({ application }) => {
+  const [isDecision, SetIsDecision] = useState("");
+  const [status, SetStatus] = useState({
+    user_ID: application.user_ID,
+    rent: application.rent,
+    decision: "",
+    dormId: application.dormId,
+  });
+
+  const acceptApplication = () => {
+    SetStatus((prevStatusInfo) => ({
+      ...prevStatusInfo,
+      decision: "accepted",
+    }));
+  };
+
+  const rejectApplication = () => {
+    SetStatus((prevStatusInfo) => ({
+      ...prevStatusInfo,
+      decision: "rejected",
+    }));
+  };
+
+  useEffect(() => {
+    if (status.decision) {
+      updateApplication();
+    }
+  }, [status.decision]);
+
+  const updateApplication = async () => {
+    try {
+      await axios.put(
+        `http://localhost:5000/listing/${application.dormId}/applications`,
+        status
+      );
+      SetStatus((prevStatusInfo) => ({
+        ...prevStatusInfo,
+        decision: "",
+      }));
+      console.log("Successfully updated application!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-row">
       <div className="w-1/3 h-full flex flex-col items-center m-2">
-        <div className=" w-full h-10 bg-[#EFEBE3] rounded"> </div>
+        <div className=" w-full h-10 bg-[#EFEBE3] rounded flex justify-center items-center">
+          {" "}
+          {application.User.fullName}{" "}
+        </div>
       </div>
       <div className="w-1/3 h-full flex flex-col items-center m-2">
-        <div className="w-full h-10 bg-[#EFEBE3] rounded"> </div>
+        <div className="w-full h-10 bg-[#EFEBE3] rounded flex justify-center items-center">
+          {" "}
+          {application.rent}{" "}
+        </div>
       </div>
       <div className="w-1/3 h-full flex items-center justify-evenly m-2 gap-2">
-        <button className="w-2/5 h-10 bg-[#8CB9BD] flex justify-center items-center rounded hover:bg-[#7FA5A8]">
+        <button
+          className="w-2/5 h-10 bg-[#8CB9BD] flex justify-center items-center rounded hover:bg-[#7FA5A8]"
+          onClick={acceptApplication}
+        >
           <FaCheck color="white" className="w-9 h-9 " />{" "}
         </button>
-        <button className="w-2/5 h-10 bg-[#B67352] flex justify-center items-center rounded hover:bg-[#8E5A41]">
+        <button
+          className="w-2/5 h-10 bg-[#B67352] flex justify-center items-center rounded hover:bg-[#8E5A41]"
+          onClick={rejectApplication}
+        >
           <ImCross color="white" className="w-8 h-8 " />{" "}
         </button>
       </div>
